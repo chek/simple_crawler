8f5f46f00c62692df8b1ab74fe0b5fea5b9990cf
Israel Gavriel <israel@commun.it>
2016-09-24 19:22:54 +0300

    test file

--- public/test.js
+++ public/test.js
@@ -1,10 +1,10 @@
 function testHomePage() {
     $.ajax({
-        type: 'POST',
-        //type: 'GET',
-        //url: 'http://localhost:3000/web_pages/list',
+        //type: 'POST',
+        type: 'GET',
+        url: '/web_pages/list',
         //url: 'http://localhost:3000/web_pages/register',
-        url: '/web_pages/parse/1',
+        //url: '/web_pages/parse/1',
         //http://884879e7.ngrok.io/web_pages/parse/980190969
        // data: {url: 'http://www.html5rocks.com/en/tutorials/developertools/async-call-stack/'},
         success: function(data) {
